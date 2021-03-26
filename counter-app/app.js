f43504a60f7de6d5f3383028eb9ae3ae9676af85
Akinkunmi <akinkunmiisrael41@gmail.com>
2021-03-26 16:04:41 +0000

    counter app done

--- counter-app/app.js
+++ counter-app/app.js
@@ -1,3 +1,13 @@
 const num = document.querySelector(".number");
 const btn1 = document.querySelector(".btn1");
 const btn2 = document.querySelector(".btn2");
+let count = 0;
+
+btn1.addEventListener("click", (e) => {
+  count -= 1;
+  num.innerHTML = count;
+});
+btn2.addEventListener("click", (e) => {
+  count += 1;
+  num.innerHTML = count;
+});
